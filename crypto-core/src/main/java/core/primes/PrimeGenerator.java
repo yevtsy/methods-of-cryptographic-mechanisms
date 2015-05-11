48e020f435a7cd471966f3143da3da5f336be214
Yevhen Tsyba <eugene.tsiba@gmail.com>
2015-05-11 11:06:07 +0300

    implemented Maurer prime generator

--- crypto-core/src/main/java/core/primes/PrimeGenerator.java
+++ crypto-core/src/main/java/core/primes/PrimeGenerator.java
@@ -1,8 +1,7 @@
 package core.primes;
 
-import core.arithmetic.Large;
-
 import java.math.BigInteger;
+import java.util.Random;
 
 /**
  * Class provides prime generator algorithms.
@@ -28,14 +27,134 @@ public class PrimeGenerator {
     private static final int radix = 2;
 
     /**
+     * The initial constellation of the table of small primes.
+     */
+    private static int[] ptab = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71};
+
+    /**
      * Generates big prime number by Maurer’s algorithm.
      *
      * @param k number of bits in generated prime number
      * @return prime number
      * @see <a href="http://en.wikipedia.org/wiki/Carmichael_number">Carmichael number</a>
+     * @see <a href="http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.89.4426&rep=rep1&type=pdf"/>
+     * @see <a href="http://s13.zetaboards.com/Crypto/topic/7234475/1/"/>
      */
-    public static Large Maurer(int k) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public static BigInteger Maurer(int k) {
+        Random rand = new Random();
+        boolean isPrime;
+
+        if (k < 20) {
+
+            while (true) {
+                int n = rand.nextInt(k);
+
+                if (n % 2 == 0 || n < 3) {
+                    continue;
+                }
+
+                final double sqrt = Math.sqrt(n);
+
+                for (int p : ptab) {
+                    if (p > sqrt) {
+                        return BigInteger.valueOf(n);
+                    }
+
+                    if (n % p == 0) {
+                        break;
+                    }
+                }
+            }
+        } else {
+            /*
+             We use c=0.005 which has been experimentally found to be optimal in processing
+             time for common PC under MS Windows and values of k of practical interest.
+             A different c value may be desirable for use in different computing
+             environments.
+             */
+
+            double c = 0.005;
+            int m = 20;
+
+            double B = c * k * k, r;
+
+            if (k > 2 * m) {
+
+                while (true) {
+                    int s = rand.nextInt();
+
+                    r = Math.pow(2, s - 1);
+
+                    if (k - k * r > m) {
+                        break;
+                    }
+                }
+            } else {
+                r = 0.5;
+            }
+
+            BigInteger q = Maurer((int) r * k);
+            BigInteger I = BigInteger.valueOf(2 ^ (k - 1)).divide(q.multiply(TWO));
+            boolean success = false;
+            BigInteger rBigInt, nBigInt = BigInteger.ZERO;
+
+            while (success == false) {
+                rBigInt = generateRandomBigInt(I.add(BigInteger.ONE), I.multiply(TWO));
+                nBigInt = TWO.multiply(rBigInt).multiply(q).add(BigInteger.ONE);
+
+                success = true;
+
+                for (int p : ptab) {
+                    if (p > B) {
+                        break;
+                    }
+
+                    if (nBigInt.remainder(BigInteger.valueOf(p)).equals(BigInteger.ZERO)) {
+                        success = false;
+                        break;
+                    }
+                }
+
+                if (!success) {
+                    continue;
+                }
+
+                int a = generateRandomIntInRange(2, nBigInt.subtract(TWO));
+
+                BigInteger b = BigInteger.valueOf(a).modPow(nBigInt.subtract(BigInteger.ONE), nBigInt);
+
+                if (b.equals(BigInteger.ONE)) {
+                    b = TWO.modPow(TWO.multiply(rBigInt), nBigInt);
+                    if (nBigInt.gcd(b.subtract(BigInteger.ONE)).equals(BigInteger.ONE)) {
+                        success = true;
+                    }
+                }
+            }
+
+            return nBigInt;
+        }
+    }
+
+    private static int generateRandomIntInRange(final int low, final BigInteger high) {
+        Random rand = new Random();
+
+        int result = rand.nextInt(Integer.MAX_VALUE);
+
+        while (result < low || BigInteger.valueOf(result).compareTo(high) >= 0) {
+            result = rand.nextInt(Integer.MAX_VALUE);
+        }
+
+        return result;
+    }
+
+    private static BigInteger generateRandomBigInt(final BigInteger low, final BigInteger high) {
+        Random rand = new Random();
+
+        BigInteger result = new BigInteger(high.bitLength(), rand);
+        while (result.compareTo(high) >= 0 || result.compareTo(low) <= 0) {
+            result = new BigInteger(high.bitLength(), rand);
+        }
+        return result;
     }
 
 
